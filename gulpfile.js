f4e00644951042a8865f80e95f9f4aa06b8f0f10
nobo <505931977@qq.com>
2017-12-04 17:52:42 +0800

    add

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var iconfont = require('gulp-iconfont');
+var runTimestamp = Math.round(Date.now()/1000);
+
+
+gulp.task("default",function() {
+    return gulp.src(['./icons/*.svg'])
+    .pipe(iconfont({
+      fontName: 'myfont', // required
+      prependUnicode: true, // recommended option
+      formats: ['ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available
+      timestamp: runTimestamp, // recommended to get consistent builds when watching files
+    }))
+      .on('glyphs', function(glyphs, options) {
+        // CSS templating, e.g.
+        console.log(glyphs, options);
+      })
+    .pipe(gulp.dest('./fonts/'));
+});
