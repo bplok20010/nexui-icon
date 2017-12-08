d60fc63affeace5149b4fd884fbbaaaf17b196fc
nobo <505931977@qq.com>
2017-12-08 19:08:28 +0800

    add test

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,32 @@ var gulp = require('gulp');
 var iconfont = require('gulp-iconfont');
 var runTimestamp = Math.round(Date.now()/1000);
 
+/**
+ * string to unicodes
+ *
+ * @param  {string} str string
+ * @return {Array}      unicodes
+ */
+function string2unicodes(str) {
+	return str.split('').map(function (text) {
+		return text.charCodeAt(0);
+	});
+}
 
 gulp.task("default",function() {
     return gulp.src(['./icons/*.svg'])
     .pipe(iconfont({
-      fontName: 'myfont', // required
-      prependUnicode: true, // recommended option
-      formats: ['ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available
+      fontName: 'nex-iconfont', // required
+      //prependUnicode: true, // recommended option
+      formats: ['ttf', 'eot', 'woff', 'svg'], // default, 'woff2' and 'svg' are available
       timestamp: runTimestamp, // recommended to get consistent builds when watching files
     }))
       .on('glyphs', function(glyphs, options) {
         // CSS templating, e.g.
+		glyphs.forEach(function(item){
+			item.unicode = item.unicode[0].charCodeAt(0);
+			//console.log('unicode:', string2unicodes(item.unicode[0]))	
+		});
         console.log(glyphs, options);
       })
     .pipe(gulp.dest('./fonts/'));
